c54a830aa0b76953425fc3fb2f02809f6f7529ec
snow1s-me <Vanzzhenya604@gmail.com>
2025-05-03 15:02:22 +0300

    Add command print

--- main.py
+++ main.py
@@ -1,39 +1,10 @@
-def add(x, y):
-    return x + y
+import math
 
-def subtract(x, y):
-    return x - y
+def solve_quadratic(a, b, c):
+    D = b**2 - 4*a*c
+    print(f"Дискримінант: D = {D}")
 
-def multiply(x, y):
-    return x * y
-
-def divide(x, y):
-    if y != 0:
-        return x / y
-    else:
-        return "Помилка: Ділення на нуль!"
-
-print("Виберіть операцію:")
-print("1. Додавання")
-print("2. Віднімання")
-print("3. Множення")
-print("4. Ділення")
-
-choice = input("Введіть номер операції (1/2/3/4): ")
-
-try:
-    num1 = float(input("Введіть перше число: "))
-    num2 = float(input("Введіть друге число: "))
-
-    if choice == '1':
-        print("Результат:", add(num1, num2))
-    elif choice == '2':
-        print("Результат:", subtract(num1, num2))
-    elif choice == '3':
-        print("Результат:", multiply(num1, num2))
-    elif choice == '4':
-        print("Результат:", divide(num1, num2))
-    else:
-        print("Неправильний вибір операції")
-except ValueError:
-    print("Помилка: потрібно вводити числа")
+    if D > 0:
+        x1 = (-b + math.sqrt(D)) / (2*a)
+        x2 = (-b - math.sqrt(D)) / (2*a)
+        print(f"Рівняння має два корені: x1 = {x1}, x2 = {x2}")

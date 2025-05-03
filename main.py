ca10fb3b7e63054af7f41c9ab48a3650d032d2bb
snow1s-me <Vanzzhenya604@gmail.com>
2025-05-03 13:19:30 +0300

    Add command print

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-print("Hello")
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Помилка: Ділення на нуль!"
+
+print("Виберіть операцію:")
+print("1. Додавання")
+print("2. Віднімання")
+print("3. Множення")
+print("4. Ділення")
+
+choice = input("Введіть номер операції (1/2/3/4): ")
+
+try:
+    num1 = float(input("Введіть перше число: "))
+    num2 = float(input("Введіть друге число: "))
+
+    if choice == '1':
+        print("Результат:", add(num1, num2))
+    elif choice == '2':
+        print("Результат:", subtract(num1, num2))
+    elif choice == '3':
+        print("Результат:", multiply(num1, num2))
+    elif choice == '4':
+        print("Результат:", divide(num1, num2))
+    else:
+        print("Неправильний вибір операції")
+except ValueError:
+    print("Помилка: потрібно вводити числа")
